3133e55213707ad933eff40a7b79989be23fcd86
Christian <chrisdrobbo@gmail.com>
2022-03-20 14:45:53 +0000

    tailwindcss and devise added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  devise_for :users
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  root "home#index"
+  root "posts#index"
 end
