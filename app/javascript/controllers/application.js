93a37890b856a5ebe14ae5012d7383f1c7478c07
Christian <chrisdrobbo@gmail.com>
2022-03-23 07:15:00 +0000

    centered create post form in dashboard view

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,7 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
+import { Dropdown } from "tailwindcss-stimulus-components"
+application.register('dropdown', Dropdown)
+
 export { application }
