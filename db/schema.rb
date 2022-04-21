8f5a3b7f23f9dd396704dcd2bfabc3f23dcf26fa
Christian Robinson <christian@pop-os.localdomain>
2022-04-21 07:35:46 +0100

    Amended new post logic to hide when user is not logged in

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_10_093143) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
@@ -22,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_10_093143) do
   end
 
   create_table "twitter_accounts", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "name"
     t.string "username"
     t.string "image"
