369828bc1f04370e63dbdf66fddf8bf0888fa39d
Christian Robinson <christian@pop-os.localdomain>
2022-04-21 17:47:47 +0100

    removed applciation.rb css compiling settings

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,5 @@ module SocialScheduler
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.assets.precompile = %w( *.js *.css )
   end
 end
