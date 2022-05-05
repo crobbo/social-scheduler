32270e708ea987eb60b5ebf00eba6072076103ed
Christian Robinson <christian@pop-os.localdomain>
2022-05-05 21:42:58 +0100

    added character counter feature

---
+++ app/javascript/controllers/tweet_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+  static targets = [ "field", "output" ]
+
+  static values = {
+    characterCount: Number,
+  }
+
+  connect() {
+    this.change();
+  }
+
+  change() {
+    let length = this.fieldTarget.value.length;
+    this.outputTarget.textContent = `${280 - length}`;
+    if(length > 280) {
+      this.outputTarget.classList.add('text-red-600');
+    } else {
+      this.outputTarget.classList.remove('text-red-600');
+    }
+  }
+}
