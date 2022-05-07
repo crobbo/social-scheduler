e665cc58313b09c904e53ead7f26d7413a77d04e
Christian Robinson <christian@pop-os.localdomain>
2022-05-07 10:03:04 +0100

    removed assets from public folder

--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -2,10 +2,7 @@ class TweetJob < ApplicationJob
   queue_as :default
 
   def perform(tweet)
-    # binding.irb
     return if tweet.published?
-    # binding.irb
-    puts tweet.schedule_time > Time.current
     return if tweet.schedule_time > Time.current
 
     tweet.publish_to_twitter!
