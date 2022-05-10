760964180df108dfa6c3ec182b3a83afd6d7a521
Christian Robinson <christian@pop-os.localdomain>
2022-05-10 21:07:57 +0100

    changed host name

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module SocialScheduler
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.action_mailer.default_url_options = { host: 'social-scheduler-app.herokuapp.com' }
+
   end
 end
